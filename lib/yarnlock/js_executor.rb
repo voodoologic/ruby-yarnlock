1f6a574fc0b3a9fd86fce8ee7d66ff0ef5f5e87e
Doug Headley <dheadl@ext.uber.com>
2020-12-02 13:03:50 -0800

    add dependency to gemspec

--- lib/yarnlock/js_executor.rb
+++ lib/yarnlock/js_executor.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
+require 'mixlib/shellout'
 module Yarnlock
   module JsExecutor
     def self.execute(script, stdin)
-      command = Mixlib::ShellOut.new("node #{script_path(script)}", cwd: Yarnlock.config.application_root)
+      command = Mixlib::ShellOut.new("node #{script_path(script)}", cwd: Yarnlock.config.application_path)
       command.run_command
       command.stdout
     end
